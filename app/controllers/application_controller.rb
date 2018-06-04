75a1781c55f5663c16e91c73d71868c8f844d0c1
bafraikin <fraikin.baptiste@gmail.com>
2018-06-04 19:25:05 +0200

    rien a changé jcrois

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ protect_from_forgery unless: -> { request.format.json? }
 cache = ActiveSupport::Cache::MemoryStore.new
 
 cache.read('city')   # => nil
-cache.write('city', "Duckburgh")
+cache.write('city', 'nique ta mere')
 puts cache.read('city')   # => "Duckburgh"
 
 
