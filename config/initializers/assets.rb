3defaa2b248ace6f6a28033239937fd42dc4aec2
Paul <broussolle.paul@gmail.com>
2018-06-05 11:22:07 +0200

    Mise a jour flat ui et créa fichier css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( place.js )
 Rails.application.config.assets.precompile += %w( map.js )
+
