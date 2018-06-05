5c403694723a1b1bf73b59200c11ed2cf885ee6d
bafraikin <fraikin.baptiste@gmail.com>
2018-06-05 10:10:11 +0200

    controller ajout d'une place en fonction de geoloc

---
+++ app/controllers/place_controller.rb
@@ -0,0 +1,11 @@
+class PlaceController < ApplicationController
+  def add_geoloc
+
+    lat = params[:"lati"]
+    lng = params[:"long"]
+
+    @a = Place.create(latitude: lat, longitude: lng)
+
+    render json {rendu: @a}
+  end
+end
