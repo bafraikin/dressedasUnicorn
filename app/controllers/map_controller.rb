f3191d396191e39ab5e8c917929bfac89b831127
bafraikin <fraikin.baptiste@gmail.com>
2018-06-05 13:33:51 +0200

    controller

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,20 +1,14 @@
-require "pry"
-
 class MapController < ApplicationController
   def index
 
-
   end
 
-  def testons
+  def explore
 
      north_lat = params[:"Nlat"].to_f
      north_long = params[:"Nlong"].to_f
      south_lat = params[:"Slat"].to_f
      south_long = params[:"Slong"].to_f
-
-
-
     # latitude = 100 / 111.111 
     # longitude =  100 / (lat * Math.cos(lat))
 
