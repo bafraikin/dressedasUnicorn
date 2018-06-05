54d4f56a5e50b588dd421ca4fe182eb63056598c
Jordane <faou.jordane@gmail.com>
2018-06-05 17:57:42 +0200

    rspec not working

--- spec/controllers/place_controller_spec.rb
+++ spec/controllers/place_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe PlaceController, type: :controller do
 
@@ -7,6 +8,20 @@ RSpec.describe PlaceController, type: :controller do
       get :add_geoloc
       expect(response).to have_http_status(:success)
     end
+  end
+
+end
+
+
+RSpec.describe PlaceController, type: :controller do
+  it 'should generate the right adress' do
+    boutique = Place.new (name: '')
+    boutique.Place.longitude << Place.longitude.new ('longitude' => Faker::Address.longitude)
+    boutique.Place.latitude << Place.latitude.new (latitude: 'Faker::Address.latitude')
+    boutique.save
+   
+    expect(Place.adress).to_eq()
+    expect(Place.adress).to_include()
   end
 
 end
