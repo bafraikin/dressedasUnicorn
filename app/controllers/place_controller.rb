6f55d77991b6fbddd1cdcface2c607c1abbd111e
bafraikin <fraikin.baptiste@gmail.com>
2018-06-06 18:46:32 +0200

    on avanceé

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -2,11 +2,14 @@ class PlaceController < ApplicationController
   before_action :authenticate_user!
 
   def add_geoloc
-
     lat = params[:"lati"]
     lng = params[:"long"]
 
     @a = Place.create(latitude: lat, longitude: lng)
     render json: {rendu: @a}
+  end
+
+  def find_with_address
+    render json: {rendu : Geocoder.coordinates(params[:"address"])}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'map#index'
   post "/", to: 'map#explore'
-  post "/add_place", to: 'place#add_geoloc'
+  post "/add_place", to: 'places#add_geoloc'
+  post "/find_address", to: "places#find_with_address"
   #root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
