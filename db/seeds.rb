90c3758595c76e57bad90d72cf9567a6d6d9f176
Selma <asakab@live.fr>
2018-06-07 19:29:53 +0200

    db shop supprimée

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,47 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#Place.create(name: "blabla", address: "chouchou")
 require 'csv'
 
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'shopz.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
-  t = Place.create
-  t.boutique = row[:Boutique]
+  t = Place.new
+  t.shop = row[:Boutique]
+  t.address = row[:Addresse]
+  t.horaire = row[:Horaire]
+  t.metro = row[:Metro]
+puts "#{t.shop}, #{t.address} saved"
+end
+
+puts "There are now #{Place.count} rows in the transactions table"
+
+ 
+
+=begin require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'shopz.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Place.new
+  t.shop = row[:Boutique]
   t.address = row[:Addresse]
   t.horaire = row[:Horaire]
   t.metro = row[:Metro]
   
   
-  puts "#{t.boutique}, #{t.address} saved"
+  create_table :places do |t|
+      t.string :name
+      t.float :longitude
+      t.float :latitude
+      t.string :address
+      t.integer :average_price
+      t.belongs_to :map
+      t.belongs_to :town
+      t.integer :tag_to_places_count
+
+  puts "#{t.shop}, #{t.address} saved"
 end
 
 puts "There are now #{Place.count} rows in the transactions table"
@@ -28,8 +56,6 @@ puts "There are now #{Place.count} rows in the transactions table"
 
 
 
-
-=begin 
 require 'faker'
 index = 0
 3.times do 
@@ -39,4 +65,5 @@ index = 0
     index += 1
   end
 end
+
 =end
