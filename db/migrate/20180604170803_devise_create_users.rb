75ecefc900ac06da9c4271de3106d379da2da057
bafraikin <fraikin.baptiste@gmail.com>
2018-06-08 16:50:09 +0200

    role admin

--- db/migrate/20180604170803_devise_create_users.rb
+++ db/migrate/20180604170803_devise_create_users.rb
@@ -7,6 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :username
+      t.boolean :admin
 
       ## Recoverable
       t.string   :reset_password_token
