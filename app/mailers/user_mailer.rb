f4a7add3b18d23c855ee17c3a501bd6d5dcb7719
Paul <broussolle.paul@gmail.com>
2018-06-08 13:40:59 +0200

    coucou

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,14 @@
 class UserMailer < ApplicationMailer
     default from: 'broussolle.paul@gmail.com'
- 
+ @mail_admin = 'urbemporium@gmail.com'
 
   def new_user_email(user)
     @user = user
-    mail(:to => 'broussolle.paul@gmail.com', :subject => "Nouvel utilisateur créé")
+    mail(:to => @mail_admin, :subject => "Nouvel utilisateur créé")
   end
 
   def deleted_user_email(user)
     @user = user
-    mail(:to => 'broussolle.paul@gmail.com', :subject => "Compte utilisateur détruit")
+    mail(:to => @mail_admin, :subject => "Compte utilisateur détruit")
   end 
 end
