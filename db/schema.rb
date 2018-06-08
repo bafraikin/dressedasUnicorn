ee03ebb2c0b5ac436d5788455304a46cc442fd30
bafraikin <fraikin.baptiste@gmail.com>
2018-06-08 17:24:04 +0200

    admin

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_06_04_170803) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "username"
+    t.boolean "admin"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
