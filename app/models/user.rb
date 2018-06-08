15a5c9fcafb0930c49ef7177f171224cd6e4ea20
Paul <broussolle.paul@gmail.com>
2018-06-08 12:53:52 +0200

    Creation du mail pour utilisateur lorsque nouvel utilisateur

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+# Cette méthode envoie un mail à l'admin lorsqu'un utilisateur a été créé
+      after_create :new_user_created_mail
+  
+  def new_user_created_mail
+    UserMailer.new_user_email(self).deliver
+  end
 end
