60d571d6c89182d2ed8ccf58171d628364621323
Paul <broussolle.paul@gmail.com>
2018-06-08 13:13:29 +0200

    Ajout mail utilisateur détruit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,8 @@ class UserMailer < ApplicationMailer
     mail(:to => 'broussolle.paul@gmail.com', :subject => "Nouvel utilisateur créé")
   end
 
+  def deleted_user_email(user)
+    @user = user
+    mail(:to => 'broussolle.paul@gmail.com', :subject => "Compte utilisateur détruit")
+  end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 # Cette méthode envoie un mail à l'admin lorsqu'un utilisateur a été créé
       after_create :new_user_created_mail
+
+      after_destroy :user_deleted_mail
+
   
   def new_user_created_mail
     UserMailer.new_user_email(self).deliver
+  end
+
+  def user_deleted_mail
+    UserMailer.deleted_user_email(self).deliver
   end
 end
