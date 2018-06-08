06fd9c97d90a508f41dbc8b3c8b98966974a1789
Paul <broussolle.paul@gmail.com>
2018-06-08 17:01:21 +0200

    coucou

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,10 +1,19 @@
 class MapController < ApplicationController
+  before_action :authenticate_user!, only: [:created]
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path, :notice => 'Tu dois être connecté pour accéder à cette page ;) '
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
 
   def index
-
-
   end
-   
+
   def list
     @places = Place.all
   end
@@ -16,20 +25,21 @@ class MapController < ApplicationController
   end
 
   def explore
-     north_lat = params[:"Nlat"].to_f
-     north_long = params[:"Nlong"].to_f
-     south_lat = params[:"Slat"].to_f
-     south_long = params[:"Slong"].to_f
-    
-    # latitude = 100 / 111.111 
+    north_lat = params[:"Nlat"].to_f
+    north_long = params[:"Nlong"].to_f
+    south_lat = params[:"Slat"].to_f
+    south_long = params[:"Slong"].to_f
+
+    # latitude = 100 / 111.111
     # longitude =  100 / (lat * Math.cos(lat))
 
     @a = Place.where("latitude <= #{north_lat} AND latitude >= #{south_lat} AND longitude <= #{south_long} AND longitude >= #{north_long}")
-    render json: {rendu: @a }
+    render json: {rendu: @a}
   end
+  
 end
 
 def find_place
-   @a = Place.find(params[:"id"])
-   redirect_to "/:#{@a.latitude}&#{@a.longitude}"
+  @a = Place.find(params[:"id"])
+  redirect_to "/:#{@a.latitude}&#{@a.longitude}"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  mount ForestLiana::Engine => '/forest'
+  mount ForestLiana::Engine => "/forest"
   devise_scope :user do
-    get '/sign-in' => "devise/sessions#new", :as => :login
+    get "/sign-in" => "devise/sessions#new", :as => :login
   end
-  get "list", to:"map#list"
-  get "created", to:"map#created"
+  resources :places
+  get "list", to: "map#list"
+  get "created", to: "map#created"
   get "test", to: "map#test"
   devise_for :users
-  root to: 'map#index'
-  post "/", to: 'map#explore'
-  post "/add_place", to: 'place#add_geoloc'
-  post "/find_address", to: 'place#find_with_address'
-  post "/put_address", to: 'place#put_address'
+  root to: "map#index"
+  post "/", to: "map#explore"
+  post "/add_place", to: "place#add_geoloc"
+  post "/find_address", to: "place#find_with_address"
+  post "/put_address", to: "place#put_address"
   #root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
