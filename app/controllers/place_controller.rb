82771174137145dbba65d6082153d3b33b69fa7b
Itspacome <pacome.perrichot@gmail.com>
2018-06-11 21:44:38 +0200

    wesh

--- app/controllers/place_controller.rb
+++ app/controllers/place_controller.rb
@@ -18,8 +18,55 @@ class PlaceController < ApplicationController
   def put_address
     lat = params[:"lati"]
     lng = params[:"long"]
-    current_user.places << Place.new(latitude: lat, longitude: lng, address: Geocoder.search([lat,lng])[0].address, name: params[:"nom"])
+    current_user.places << Place.new(latitude: lat, longitude: lng, address: Geocoder.search([lat,lng])[0].address, name: params[:"nom"], description: params["description"])
     @a = current_user.places.last
  render json: {put: @a}
   end
+
+  def new
+        @place = Place.new
+    end
+    
+
+    def create
+      @place = Place.new(place_params)
+     
+      place.save
+
+    end
+  
+    
+    def show
+      @place = Place.find(params[:id])
+    end
+
+
+    def edit
+      @place = Place.find(params[:id])
+    end
+
+
+  def destroy
+      @place = Place.find(params[:id])
+      @place.destroy
+      redirect_to '/created'
+      flash[:notice] = "Boutique supprimée !"
+  end
+
+  #def add_place
+
+   # if @place == nil 
+
+
+  #end 
+
+
+private
+
+    def place_params
+      params.require(:place).permit(:name, :longitude, :latitude, :address, :average_price, :description, :map, :town, :creator,
+:tag_to_places_count)
+    end
+
+
 end
