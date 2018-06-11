5f71f8a3e641bbc5ca3846b3b0ebc00dbe84fd44
Selma <asakab@live.fr>
2018-06-11 18:44:59 +0200

    shop delete + username unique + ajout new shop

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "/sign-in" => "devise/sessions#new", :as => :login
   end
-  resources :place
+  resources :place, only: [:destroy]
   get "list", to: "map#list"
   get "created", to: "map#created"
+  get "edit", to: "map#edit"
   get "test", to: "map#test"
   devise_for :users
   root to: "map#index"
@@ -13,6 +14,7 @@ Rails.application.routes.draw do
   post "/add_place", to: "place#add_geoloc"
   post "/find_address", to: "place#find_with_address"
   post "/put_address", to: "place#put_address"
+
   #root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
