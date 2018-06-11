fac4fa5d0eadd5ff0f4c130f4c3d6d7c30392ae9
Itspacome <pacome.perrichot@gmail.com>
2018-06-11 21:51:13 +0200

    ok

--- db/migrate/20180604170803_devise_create_users.rb
+++ db/migrate/20180604170803_devise_create_users.rb
@@ -2,7 +2,7 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users :admin :boolean :default => 0 do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
--- db/migrate/20180608120817_add_role_to_users.rb
+++
@@ -1,4 +0,0 @@
-class AddRoleToUsers < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
