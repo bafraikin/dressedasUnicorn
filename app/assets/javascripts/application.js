2632a463337ec3781837092cde3c82e4509874c2
Paul <broussolle.paul@gmail.com>
2018-06-11 15:41:57 +0200

    ajout de responsive clavier

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -83,4 +83,19 @@ function closeModal() {
         }
 }
 
+var $htmlOrBody = $('html, body'), // scrollTop works on <body> for some browsers, <html> for others
+    scrollTopPadding = 8;
 
+$('textarea').focus(function() {
+    // get textarea's offset top position
+    var textareaTop = $(this).offset().top;
+    // scroll to the textarea
+    $htmlOrBody.scrollTop(textareaTop - scrollTopPadding);
+});
+
+$('input').focus(function() {
+    // get textarea's offset top position
+    var textareaTop = $(this).offset().top;
+    // scroll to the textarea
+    $htmlOrBody.scrollTop(textareaTop - scrollTopPadding);
+});
