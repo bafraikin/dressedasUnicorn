8ae92ba73c70cf4938dcf3c3626441e2256d569a
Jordane <faou.jordane@gmail.com>
2018-06-13 13:30:20 +0200

    trying to tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_06_12_142145) do
-=======
 ActiveRecord::Schema.define(version: 2018_06_12_164954) do
->>>>>>> 4539a6654ee33a0dc0b31311869e9b64cc4887f6
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
