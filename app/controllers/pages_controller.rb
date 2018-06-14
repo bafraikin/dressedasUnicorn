7686c536ec76ee08f1f755cca26928ca5742a646
bafraikin <fraikin.baptiste@gmail.com>
2018-06-14 10:14:14 +0200

    coucou

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,15 +12,6 @@ class PagesController < ApplicationController
   end
 
   def list
-    @places = Place.all
-    @creators = []
-    @ids = []
-    @places.each_with_index do |place, index|
-      @creators[index] = place.creators[0].username
-    end
-    current_user.liked_places.each_with_index do |like,index |
-      @ids[index] = like.id
-    end
   end
 
   def test
