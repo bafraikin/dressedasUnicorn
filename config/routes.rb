fd9d956cf63442ce40c3b51a600478ea53513aa0
Selma <asakab@live.fr>
2018-06-14 19:27:11 +0200

    jpp..

--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,7 @@ Rails.application.routes.draw do
 
   #root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #resources :contacts, only: [:new, :create]
+  
 end
